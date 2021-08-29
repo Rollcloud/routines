c41be6432e272957d076c91b8b064612af882e65
Rollcloud <rollcloud@gmail.com>
2021-08-29 14:21:39 +0200

    Improve responsive rendering of routines

--- js/artists/routine-artist.js
+++ js/artists/routine-artist.js
@@ -5,23 +5,31 @@ import { deleteElementInList, moveUpIndexInList } from "./base-artist.js";
 import { renderHabitListItems, renderHabitCards } from "./habits-artist.js";
 
 // Components
-const routineName = document.getElementById("routine-name");
-const routineHabitsList = document.getElementById("routine-habits");
+const routineNames = document.querySelectorAll(".routine-name");
+const routineHabitsLists = document.querySelectorAll(".routine-habits");
 const routineHabitsCards = document.getElementById("routine-habits-scroller");
 
 function renderRoutine(detail) {
-  routineName.textContent = detail.routine.name;
-  renderHabitListItems(detail.routine.habits, routineHabitsList);
-  renderHabitCards(detail.routine.habits, routineHabitsCards);
+  routineNames.forEach((routineName) => {
+    routineName.textContent = detail.routine.name;
+  });
+  routineHabitsLists.forEach((routineHabitsList) => {
+    renderHabitListItems(detail.routine.habits, routineHabitsList);
+  });
+  if (routineHabitsCards) renderHabitCards(detail.routine.habits, routineHabitsCards);
 }
 
 function moveUpHabitInRoutine(detail) {
-  moveUpIndexInList(detail.habitIdx, routineHabitsList);
+  routineHabitsLists.forEach((routineHabitsList) => {
+    moveUpIndexInList(detail.habitIdx, routineHabitsList);
+  });
 }
 
 function removeHabitFromRoutine(detail) {
-  const habitElement = routineHabitsList.children[detail.habitIdx];
-  deleteElementInList(habitElement);
+  routineHabitsLists.forEach((routineHabitsList) => {
+    const habitElement = routineHabitsList.children[detail.habitIdx];
+    deleteElementInList(habitElement);
+  });
 }
 
 addCustomListener("routineRetrieved", renderRoutine);
